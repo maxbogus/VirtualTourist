91facbcae665281c2b6c8e55040f292864c6b8f0
Max Boguslavskiy <max.bogus@gmail.com>
2018-05-04 01:45:07 +0300

    setup for photo album and travel location map controller

---
+++ VirtualTourist/View Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Max Boguslavskiy on 04/05/2018.
+//  Copyright © 2018 Max Boguslavskiy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
--- VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
+++ VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelLocationsMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Max Boguslavskiy on 02/05/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TravelLocationsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
