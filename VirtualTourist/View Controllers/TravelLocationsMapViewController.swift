32b90eec00e9e47a4d32425ab2a7f794af17e2df
Max Boguslavskiy <max.bogus@gmail.com>
2018-05-09 03:35:19 +0300

    long hold on map adds pin

--- VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
+++ VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
@@ -12,11 +12,20 @@ import UIKit
 class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    var myAnnotations = [CLLocation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(addWaypoint(longGesture:)))
+        mapView.addGestureRecognizer(longGesture)
         mapView.showsUserLocation = true
     }
-
+    
+    @objc func addWaypoint(longGesture: UIGestureRecognizer) {
+        let touchPoint = longGesture.location(in: mapView)
+        let newCoordinates = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinates
+        mapView.addAnnotation(annotation)
+    }
 }
-
